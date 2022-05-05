9504eaa388d88cf641265e3a549ae912e159f8d8
ht18 <herschel.tuil@gmail.com>
2022-05-05 12:15:01 +0200

    just add suggestions

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let receipts = ["Apple pie", "Tiramisu","Cheesecake"];
+let receipts = {
+    applePie: 'Apple pie',
+    tiramisu: 'Tiramisu',
+    cheesecake: 'Cheesecake'
+};
 
 const searchinput = document.getElementById('searchInput');
 
-searchinput.addEventListener('keyup', function(){
+searchinput.addEventListener("keyup", function(){
     const input = searchinput.value;
 
-    const result = receipts.filter(item => item.name.includes(input));
-})
+    const result = receipts.filter(item => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()));
+    
+    let suggestion = '';
+
+    result.forEach(resultItem => 
+        suggestion +=`
+            <div class="suggestion">$(resultItem.name)</div>`
+    )
+
+    document.getElementById('suggestions').innerHTML = suggestion;
+    })
