0411a73837bd19308eaf66a2d45ad7d0fd5232a7
ht18 <herschel.tuil@gmail.com>
2022-05-07 10:09:08 +0200

    Version 1.05
    add div "suggestions" and scrool bar inside this div

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-let receipts = {
-    applePie: 'Apple pie',
-    tiramisu: 'Tiramisu',
-    cheesecake: 'Cheesecake'
-};
+let receipts = [ /*création d'un tableau d'objets*/
+    {name:'Tiramisu', sugar:35, tomatoe:0, cheese:300, speculos:0, apple:0}, 
+    {name:'Cheesecake', sugar:35, tomatoe:0, cheese:200, speculos:10, apple:0},
+    {name:'Apple Pie', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4},
+    {name:'test1', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4},
+    {name:'test2', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4},
+    {name:'test', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4},
+    {name:'test3', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4},
+    {name:'test4', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4},
+    {name:'test5', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4},
+    {name:'test6', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4},
+    {name:'test7', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4},
+    {name:'test8', sugar:35,tomatoe:0, cheese:0, speculos:0, apple:4}
+]; 
 
-const searchinput = document.getElementById('searchInput');
 
-searchinput.addEventListener("keyup", function(){
-    const input = searchinput.value;
+const searchinput = document.getElementById('searchInput'); /* création d'une constante qui sélectionne searchInput */ 
 
-    const result = receipts.filter(item => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()));
+searchinput.addEventListener('keyup', function(){
+    const input = searchinput.value; /* input = constante = valeur entrée par l'utilisateur */
+
+    const result = receipts.filter(item => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase())); /* => désigne une fonction anonyme/ item = objet / toLocaleLowercase = convertit en minuscule */
     
-    let suggestion = '';
-
-    result.forEach(resultItem => 
+    let suggestion = ''; /* on commence à créer notre liste de suggestion en fonction du filtre appliqué */
+    
+    result.forEach(resultItem => /* forEach = appliquer la fonction sur chaque élément du tableau */
         suggestion +=`
-            <div class="suggestion">$(resultItem.name)</div>`
+            <div class="suggestion">${resultItem.name}</div>
+        `
     )
 
     document.getElementById('suggestions').innerHTML = suggestion;
-    })
+
+})
